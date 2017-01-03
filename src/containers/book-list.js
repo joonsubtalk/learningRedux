806734edded69772c539cdd5d62654c3a0d3cd34
joonsub <joonsubtalk@gmail.com>
2017-01-02 23:04:16 -0800

    Added reducers and actions for redux app

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';    //import from redux to glue them together
+import {selectBook} from '../actions/index';
+import {bindActionCreators} from 'redux';
 
 class BookList extends Component {
     renderList () {
         return this.props.books.map((book) => {
             return (
-                <li key={book.title} className="list-group-item">
+                <li
+                    key={book.title}
+                    onClick={() => this.props.selectBook(book)}
+                    className="list-group-item">
                     {book.title}
                 </li>
             )
@@ -29,5 +34,13 @@ function mapStateToProps(state) {
     };
 }
 
+// anything returned from this function will end up as props on the booklist container
+function mapDispatchToProps(dispatch) {
+    // whenever selectbook is called, the result should be passed to all of our reducers
+    return bindActionCreators({selectBook: selectBook}, dispatch)
+}
+
 //connect() takes function and component to make container
-export default connect(mapStateToProps)(BookList);
+// promote booklist from a component to a container that needs to know about this new dispatch
+// method, selectBook and make available as a prop
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
